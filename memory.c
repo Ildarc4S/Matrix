8adc97ab4ce835cae352247de4e200b566135276
Ildarc4S <ildarc4S@gmail.com>
2025-01-22 13:06:29 +0300

    Create and free matrix functions

--- memory.c
+++ memory.c
@@ -3,7 +3,35 @@
 
 int s21_create_matrix(int rows, int columns, matrix_t *result) {
   S21OperationsResultCode result_code = kCodeOK;
+  if (rows <= 0 || columns <= 0 || result == NULL) {
+    /*if (result != NULL) {*/
+      /*result->matrix = NULL;*/
+    /*}*/
+    return kCodeIncorrect;
+  }
+
   result->matrix = (double**)calloc(rows, sizeof(double*));
+  if (result->matrix != NULL) {
+    for (int i = 0; i < rows; i++) {
+      result->matrix[i] = (double*)calloc(columns, sizeof(double));
+    }
+    result->columns = columns;
+    result->rows = rows;
+  } else {
+    result_code = kCodeIncorrect;
+  }
+
+  return result_code;
 }
-void s21_remove_matrix(matrix_t *A);
+
+void s21_remove_matrix(matrix_t *A) {
+  for (int i = 0; i < A->rows; i++) {
+    free(A->matrix[i]);
+    A->matrix[i] = NULL;
+  }
+  free(A->matrix);
+  A->matrix = NULL;
+  A->columns = 0;
+  A->rows = 0;
+}
 
