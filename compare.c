57f52e8975dbb14e4f7960c4e9457357b651fd27
Ildarc4S <ildarc4S@gmail.com>
2025-01-25 01:24:42 +0300

    Fix compare

--- compare.c
+++ compare.c
@@ -1,15 +1,17 @@
 #include "compare.h"
 
 int s21_eq_matrix(matrix_t *A, matrix_t *B) {
-  if (A == NULL || B == NULL || !s21_size_eq(A, B)) {
+  if (A == NULL || B == NULL ) {
     return FAILURE;
   }
-
+  if (!s21_size_eq(A, B)) {
+    return FAILURE;
+  }
   int result_code = SUCCESS;
 
   int stop = 0;
-  for (int i = 0; i < A->rows && !stop; i++) {
-    for (int j = 0; j < A->columns && !stop; i++) {
+  for (int i = 0; i < A->rows && stop == 0; i++) {
+    for (int j = 0; j < A->columns && stop == 0; j++) {
       if (fabs(A->matrix[i][j] - B->matrix[i][j]) > 1e-7) {
         result_code = FAILURE;
         stop = 1;
