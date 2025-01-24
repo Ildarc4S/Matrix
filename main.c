0192e3255e4e5c47a74e8c980bee24fb4dd2b5ba
Ildarc4S <ildarc4S@gmail.com>
2025-01-25 01:25:56 +0300

    Testing

--- main.c
+++ main.c
@@ -5,20 +5,31 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include "test.h"
 
 int main() {
-  matrix_t A;
-  s21_create_matrix(4, 4, &A); 
-  for (int i = 0; i < A.rows; i++) {
-    for (int j = 0; j < A.columns; j++) {
-      A.matrix[i][j] = rand() / (double)RAND_MAX;
-    }
-  }
-  s21_print_matrix(&A);
-  double res = 0.0;
-  s21_determinant(&A, &res);
-  printf("%f\n", res); 
-  s21_print_matrix(&A);
+ // success with task reference values
+  matrix_t A = {};
+  matrix_t result = {};
+  matrix_t eq_matrix = {};
+  s21_create_matrix(3, 3, &A);
+  s21_create_matrix(3, 3, &eq_matrix);
+  A.matrix[0][0] = 2, A.matrix[0][1] = 5, A.matrix[0][2] = 7;
+  A.matrix[1][0] = 6, A.matrix[1][1] = 3, A.matrix[1][2] = 4;
+  A.matrix[2][0] = 5, A.matrix[2][1] = -2, A.matrix[2][2] = -3;
+  eq_matrix.matrix[0][0] = 1, eq_matrix.matrix[0][1] = -1,
+  eq_matrix.matrix[0][2] = 1;
+  eq_matrix.matrix[1][0] = -38, eq_matrix.matrix[1][1] = 41,
+  eq_matrix.matrix[1][2] = -34;
+  eq_matrix.matrix[2][0] = 27, eq_matrix.matrix[2][1] = -29,
+  eq_matrix.matrix[2][2] = 24;
+  printf("%d\n", s21_inverse_matrix(&A, &result)); //, OK);
+  printf("%d\n", s21_eq_matrix(&result, &eq_matrix)); // SUCCESS);
+  s21_print_matrix(&result);
+  printf("\n");
+  s21_print_matrix(&eq_matrix);
   s21_remove_matrix(&A);
+  s21_remove_matrix(&result);
+  s21_remove_matrix(&eq_matrix);
   return 0;
 }
